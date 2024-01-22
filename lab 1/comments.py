53ebb26ca8153235cb9ed665c5aa38de4ded310b
Beknur <beka.com>
2024-01-23 02:38:48 +0600

    added comments

--- lab 1/comments.py
+++ lab 1/comments.py
@@ -1,2 +1,7 @@
 #ex1
 #This is a comment
+
+#ex 2
+'''This is a comment
+written in 
+more than just one line '''
--- lab 1/syntax.py
+++ lab 1/syntax.py
@@ -3,7 +3,5 @@ print("Hello world!")
 
 # ex 2
 if 5 > 2:
-    print("YES")
- #ex3
-#This is a comment   
+    print("YES") 
 
